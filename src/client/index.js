8c4781f72d71680c03204237ba97fd08df6995d8
brianlim89 <br.lim89@gmail.com>
2023-02-19 13:30:58 -0800

    google oauth basic login

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <GoogleOAuthProvider clientId="3488776675-f0qp9cl1dprjjiv1htfic8v0idon876g.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
+);
