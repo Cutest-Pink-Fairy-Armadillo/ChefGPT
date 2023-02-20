4b7a054bc012c38386d1819fd32ecf2a16d4d0cc
brianlim89 <br.lim89@gmail.com>
2023-02-19 16:03:14 -0800

    strict mode

--- src/client/index.js
+++ src/client/index.js
@@ -6,8 +6,6 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <GoogleOAuthProvider clientId="3488776675-f0qp9cl1dprjjiv1htfic8v0idon876g.apps.googleusercontent.com">
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </GoogleOAuthProvider>
 );
